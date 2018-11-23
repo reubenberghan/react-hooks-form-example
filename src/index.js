3d697dbd232ffa8ddd598b2f7441980af3a54a6b
Reuben Berghan <reuben.berghan@gmail.com>
2018-11-23 19:49:42 +1300

    Pull button out of document and simulate click

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import 'raf/polyfill'
+
 import * as React from 'react'
 import { useState } from 'react'
 import { render } from 'react-dom'
+import ReactTestUtils from 'react-dom/test-utils'
 
 import './index.css'
 // import App from './App'
-import * as serviceWorker from './serviceWorker'
+// import * as serviceWorker from './serviceWorker'
 
 function App () {
   const [count, setCount] = useState(0)
+
+  count
 
   return (
     <div>
@@ -21,7 +26,11 @@ document.body.innerHTML += `<div id="root"></div>`
 
 render(<App />, document.getElementById('root'))
 
+const button = document.getElementsByTagName('button')[0]
+
+ReactTestUtils.Simulate.click(button)
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// serviceWorker.unregister()

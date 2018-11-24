030911f9990b79ac91121f75f63eba5f07cccd21
Reuben Berghan <reuben.berghan@gmail.com>
2018-11-24 16:08:19 +1300

    Add Form with useReducer

---
+++ src/components/Form/index.js
@@ -0,0 +1,60 @@
+import * as React from 'react'
+import { useReducer } from 'react'
+
+const NAME_CHANGED = 'NAME_CHANGED'
+const EMAIL_CHANGED = 'EMAIL_CHANGED'
+const initialState = {
+  name: '',
+  email: ''
+}
+
+function reducer (state, { type, payload }) {
+  switch (type) {
+    case NAME_CHANGED:
+      return {
+        ...state,
+        name: payload.value
+      }
+    case EMAIL_CHANGED:
+      return {
+        ...state,
+        email: payload.value
+      }
+    default:
+      return state
+  }
+}
+
+export default function Form () {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { name, email } = state
+
+  function makeHandleOnChange (type) {
+    return function handleOnChange (e) {
+      const { value } = e.target
+
+      return dispatch({ type, payload: { value } })
+    }
+  }
+
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <label htmlFor='name'>
+        Name:
+        <input
+          type='text'
+          onChange={makeHandleOnChange(NAME_CHANGED)}
+          value={name}
+        />
+      </label>
+      <label htmlFor='email'>
+        Email:
+        <input
+          type='email'
+          onChange={makeHandleOnChange(EMAIL_CHANGED)}
+          value={email}
+        />
+      </label>
+    </form>
+  )
+}

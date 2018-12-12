a9bbbf9d48e580ed3cff60192dccaa953526d558
Reuben Berghan <reuben.berghan@gmail.com>
2018-12-13 07:55:16 +1300

    Added Message component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
 
 // import Form from '../Form'
+import Message from '../Message'
 import PasswordField from '../PasswordField'
 
 export default function App () {
   return (
     <>
+      <Message message={`Hello's world`} />
       <PasswordField />
     </>
   )
---
+++ src/components/Message/index.js
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export default function Message ({ message }) {
+  return <div>{message}</div>
+}
